fe72d8ca3ff544d52d6483b3f592eb0b3b5129ce
Marko Atanasievski <markoatana@gmail.com>
2024-03-07 10:37:44 +0100

    fix: remove gas limit check

--- consensus/misc/gaslimit.go
+++ consensus/misc/gaslimit.go
@@ -30,10 +30,10 @@ func VerifyGaslimit(parentGasLimit, headerGasLimit uint64) error {
 	if diff < 0 {
 		diff *= -1
 	}
-	limit := parentGasLimit / params.GasLimitBoundDivisor
-	if uint64(diff) >= limit {
-		return fmt.Errorf("invalid gas limit: have %d, want %d +-= %d", headerGasLimit, parentGasLimit, limit-1)
-	}
+	// limit := parentGasLimit / params.GasLimitBoundDivisor
+	// if uint64(diff) >= limit {
+	// 	return fmt.Errorf("invalid gas limit: have %d, want %d +-= %d", headerGasLimit, parentGasLimit, limit-1)
+	// }
 	if headerGasLimit < params.MinGasLimit {
 		return fmt.Errorf("invalid gas limit below %d", params.MinGasLimit)
 	}
